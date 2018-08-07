92855f5b7a9a3c107a2fb220d85ecbab7b593f47
Jeff Ching <chingor@google.com>
2018-08-06 17:11:10 -0700

    Run python2 -m pip

--- synth.py
+++ synth.py
@@ -28,7 +28,7 @@ repository = gcp.RepositoryGenerator(
 
 commands = [
     "mkdir -p output_dir",
-    "pip install django==1.8.12 httplib2 google-apputils python-gflags google-api-python-client",
+    "python2 -m pip install django==1.8.12 httplib2 google-apputils python-gflags google-api-python-client",
 ]
 
 # run the generator for each discovery json file
